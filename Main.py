4e002692fc1839eac1729d8d12ef21d3a595b125
xsha256 <43494720+xsha256@users.noreply.github.com>
2023-04-13 12:14:39 +0200

    Añadir tabla y colores

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import os
+from prettytable import PrettyTable
+from termcolor import colored
 from EjerciciosAntonio import Ejercicio1
 from EjerciciosAntonio import Ejercicio2
 from EjerciciosAntonio import Ejercicio3
@@ -12,20 +14,43 @@ from ejerciciosM import ejercicio10
 from ejerciciosM import ejercicio11
 
 while True:
-    os.system("cls")
-    print("Bienvenidos")
-    print("Menu principal")
-    print("1-Crea un programa en Python que acepte una fecha como cadena de caracteres")
-    print("2-Crea un programa que le pregunte al usuario su zona horaria y le muestre la hora actual en esa zona.")
-    print("3-Crea un programa en Python que tome una cadena de caracteres y devuelva el número de palabras que contiene. ")
-    print("4-Crea un programa que le pida al usuario una hora en formato hh:mm y luego calcule y muestre la hora en 24 horas")
-    print("5-Crea un programa en Python que acepte una cadena de caracteres y devuelva la cadena invertida")
-    print("6-Crea un programa que le pida al usuario un número entero y luego calcule y muestre la suma de todos los números desde 1 hasta el número ingresado.")
-    print("7-Crea un programa que le pida al usuario una cadena de caracteres y luego imprima cada carácter en una línea separada.")
-    print("8-Crea un programa en Python que acepte una lista de cadenas de caracteres y devuelva una nueva lista que contenga solo las cadenas que tienen más de 5 caracteres. ")
-    print("9-Crea un programa en Python donde la entrada sean una cadena de caracteres, una palabra y una palabra de reemplazo ,el resultado debe ser la cadena con todas las ocurrencias de la palabra reemplazadas por otra palabra.")
-    print("10-Crea un un programa en Python que tome una lista de cadenas de caracteres y devuelva una nueva lista que contenga solo las cadenas que contienen al menos una vocal.")
-    print("0 -Salir")
+    os.system("clear")
+    # print("Bienvenidos")
+    # print("Menu principal")
+    # tabla = PrettyTable()
+    # tabla.field_names = ["#", "Descripción"]
+    # tabla.add_row([colored("1", "blue"), colored("1-Crea un programa en Python que acepte una fecha como cadena de caracteres", "green")])
+    # print("2-Crea un programa que le pregunte al usuario su zona horaria y le muestre la hora actual en esa zona.")
+    # print("3-Crea un programa en Python que tome una cadena de caracteres y devuelva el número de palabras que contiene. ")
+    # print("4-Crea un programa que le pida al usuario una hora en formato hh:mm y luego calcule y muestre la hora en 24 horas")
+    # print("5-Crea un programa en Python que acepte una cadena de caracteres y devuelva la cadena invertida")
+    # print("6-Crea un programa que le pida al usuario un número entero y luego calcule y muestre la suma de todos los números desde 1 hasta el número ingresado.")
+    # print("7-Crea un programa que le pida al usuario una cadena de caracteres y luego imprima cada carácter en una línea separada.")
+    # print("8-Crea un programa en Python que acepte una lista de cadenas de caracteres y devuelva una nueva lista que contenga solo las cadenas que tienen más de 5 caracteres. ")
+    # print("9-Crea un programa en Python donde la entrada sean una cadena de caracteres, una palabra y una palabra de reemplazo ,el resultado debe ser la cadena con todas las ocurrencias de la palabra reemplazadas por otra palabra.")
+    # print("10-Crea un un programa en Python que tome una lista de cadenas de caracteres y devuelva una nueva lista que contenga solo las cadenas que contienen al menos una vocal.")
+
+    tabla = PrettyTable()
+    tabla.field_names = ["Bienvenidos"]
+    tabla.align["Bienvenidos"] = "c"
+    tabla.add_row([colored("Menu principal", "red")])
+    print(colored(tabla.get_string(), "red"))
+
+    tabla = PrettyTable()
+    tabla.field_names = ["#", "Descripción"]
+    tabla.align["Descripción"] = "l"
+    tabla.add_row([colored("1", "blue"), colored("Fecha como cadena de caracteres", "green")])
+    tabla.add_row([colored("2", "blue"), colored("Busca tu zona horaria y la hora.", "green")])
+    tabla.add_row([colored("3", "blue"), colored("Calcula el número de palabras que contiene una cadena.", "green")])
+    tabla.add_row([colored("4", "blue"), colored("Convertir la hora en 24 horas.", "green")])
+    tabla.add_row([colored("5", "blue"), colored("Invierte una cadena", "green")])
+    tabla.add_row([colored("6", "blue"), colored("Calcular del numero ingresado hasta 1.", "green")])
+    tabla.add_row([colored("7", "blue"), colored("Imprime cada caracter en una cadena separada.", "green")])
+    tabla.add_row([colored("8", "blue"), colored("Seleccionar las cadenas que tiene más de 5 caracteres.", "green")])
+    tabla.add_row([colored("9", "blue"), colored("reemplazar palabras.", "green")])
+    tabla.add_row([colored("10", "blue"), colored("Crea una lista con cadenas que tengan vocales.", "green")])
+    tabla.add_row([colored("0", "blue"), colored("Salir", "green")])
+    print(colored(tabla.get_string(), "blue"))
     opcion = input("Seleccione una opción:")
     if opcion == "1":
         Ejercicio1.principal()
